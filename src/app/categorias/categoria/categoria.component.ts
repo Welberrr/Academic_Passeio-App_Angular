6340aeaa76d56460926c18e8378b65b0403fef47
Welberrr <welberhenrique.costa@gmail.com>
2025-10-27 23:18:24 -0300

    Feat: Aplicando validacao no formulario

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
-  selector: 'app-categoria.component',
+  selector: 'app-categoria',
   standalone: false,
+  
   templateUrl: './categoria.component.html',
-  styleUrl: './categoria.component.scss',
+  styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
   camposForm: FormGroup;
 
   constructor(){
     this.camposForm = new FormGroup({
-      nome: new FormControl('', Validators.required),
-      descricao: new FormControl('', Validators.required)
-    })
+        nome: new FormControl('', Validators.required),
+        descricao: new FormControl('', Validators.required)
+    });
   }
 
   salvar(){
-    console.log('Valores digitados ', this.camposForm.value)
-    console.log('Esta valido? ', this.camposForm.valid)
+    this.camposForm.markAllAsTouched();
+
+    if(this.camposForm.valid){
+      console.log('valores digitados: ', this.camposForm.value)
+    }
+  }
+
+  isCampoInvalido(nomeCampo: string) : boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return campo?.invalid && campo?.touched && campo?.errors?.['required']
   }
 }
--- src/app/categorias/categorias.module.ts
+++ src/app/categorias/categorias.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms' // <-- Isso está correto
+import { ReactiveFormsModule } from '@angular/forms'
 
-import { CategoriasModuleRoutingModule } from './categorias-routing.module';
+import { CategoriasRoutingModule } from './categorias-routing.module';
 import { CategoriaComponent } from './categoria/categoria.component';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { CategoriaComponent } from './categoria/categoria.component';
   ],
   imports: [
     CommonModule,
-    CategoriasModuleRoutingModule,
+    CategoriasRoutingModule,
     ReactiveFormsModule
   ]
 })
