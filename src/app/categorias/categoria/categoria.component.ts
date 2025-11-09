c63be5293317e1b05219d1b9dceccb2f52bda83e
Welberrr <welberhenrique.costa@gmail.com>
2025-11-08 23:35:50 -0300

    Feat: Cadastrando categorias na api atraves da aplicacao

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { CategoriaService } from '../categoria.service';
 
 @Component({
   selector: 'app-categoria',
@@ -11,7 +12,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 export class CategoriaComponent {
   camposForm: FormGroup;
 
-  constructor(){
+  constructor(private service: CategoriaService) {
     this.camposForm = new FormGroup({
         nome: new FormControl('', Validators.required),
         descricao: new FormControl('', Validators.required)
@@ -22,7 +23,10 @@ export class CategoriaComponent {
     this.camposForm.markAllAsTouched();
 
     if(this.camposForm.valid){
-      console.log('valores digitados: ', this.camposForm.value)
+      this.service.salvar(this.camposForm.value).subscribe(
+        categoria => {
+          this.camposForm.reset();
+      })
     }
   }
 
