0623fb95ece3dc1371e0c585b5e800b0ec0643e7
Welberrr <welberhenrique.costa@gmail.com>
2025-11-15 21:20:42 -0300

    Feat: Atualizando modulo de galeria

--- src/app/galeria/galeria.module.ts
+++ src/app/galeria/galeria.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { GaleriaRoutingModule } from './galeria-routing-module';
+import { GaleriaRoutingModule } from './galeria-routing.module';
 
 
 @NgModule({
